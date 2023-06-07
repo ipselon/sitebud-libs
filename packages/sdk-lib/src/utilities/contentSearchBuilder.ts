5a9e0496e08981e04d6d5c05cf0854d7e57f2a92
ipselon <apustovalov@gmail.com>
2023-06-07 18:43:11 +0300

    Fix search builder

--- packages/sdk-lib/src/utilities/contentSearchBuilder.ts
+++ packages/sdk-lib/src/utilities/contentSearchBuilder.ts
@@ -25,20 +25,28 @@ function extractTextFromHtml(htmlString: string): string {
 
 export function buildSearchText(documentContent: DocumentContent_Bean): string {
     let resultText: string = '';
+    let restrictedAreaSection = false;
     iterateDocumentContentAreas(documentContent.documentAreas, (area: DocumentContentArea) => {
+        restrictedAreaSection = false;
         iterateDocumentContentAreaBlocks(area.blocks, (block) => {
-            iterateDocumentContentAreaBlockComponents(block.components, (component) => {
-                iterateDocumentContentAreaBlockComponentInstances(component.instances, (instance) => {
-                    for (const instancePropItem of Object.entries(instance.props)) {
-                        const [propName, propValue] = instancePropItem;
-                        if (propValue.type === 'HeaderText' || propValue.type === 'ParagraphText') {
-                            resultText += extractTextFromHtml((propValue.fieldContent as HeaderText | ParagraphText).htmlText) + '|c|';
-                        } else if (propValue.type === 'StringValue') {
-                            resultText += (propValue.fieldContent as StringValue).value + '|c|';
+            if (block.accessLevel && block.accessLevel > 0) {
+                restrictedAreaSection = true;
+            }
+            if (!restrictedAreaSection) {
+                iterateDocumentContentAreaBlockComponents(block.components, (component) => {
+                    iterateDocumentContentAreaBlockComponentInstances(component.instances, (instance) => {
+                        for (const instancePropItem of Object.entries(instance.props)) {
+                            const [propName, propValue] = instancePropItem;
+                            if (propValue.type === 'HeaderText' || propValue.type === 'ParagraphText') {
+                                resultText += extractTextFromHtml((propValue.fieldContent as HeaderText | ParagraphText).htmlText) + '|c|';
+                            }
+                            // else if (propValue.type === 'StringValue') {
+                            //     resultText += (propValue.fieldContent as StringValue).value + '|c|';
+                            // }
                         }
-                    }
+                    });
                 });
-            });
+            }
         });
     });
     if (resultText.length > 3) {
